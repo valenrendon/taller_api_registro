55e66c63c0747eae34a6572fc40f79118aa7cf42
Valentina Rendon <rendonvalentina136@gmail.com>
2025-10-07 21:36:34 -0500

    feat: Commit inicial del proyecto API

---
+++ main.py
@@ -0,0 +1,47 @@
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+import database
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # permite cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Usuario(BaseModel):
+    email: str
+    password: str
+
+# Ruta para servir front.html
+@app.get("/", response_class=HTMLResponse)
+def home():
+    with open("front.html", "r", encoding="utf-8") as f:
+        return f.read()
+
+@app.post("/register")
+def register(usuario: Usuario):
+    database.usuarios.append(usuario.dict())
+    return {"mensaje": "Usuario registrado", "usuario": usuario.dict()}
+
+@app.post("/login")
+def login(usuario: Usuario):
+    if usuario.email == "user@test.com" and usuario.password == "Test123":
+        return {"mensaje": "Login exitoso"}
+
+    for u in database.usuarios:
+        if u["email"] == usuario.email and u["password"] == usuario.password:
+            return {"mensaje": "Login exitoso"}
+
+    return {"error": "Credenciales inválidas"}
+
+
+
+
+
